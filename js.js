74b1e817a7f9b797d6a2f37e5fdf45cab0486de8
legarifulin <70882252+legarifulin@users.noreply.github.com>
2021-04-19 11:50:00 +0300

    Js part

--- js.js
+++ js.js
@@ -1,34 +1,33 @@
+// ================ Select elements =================
+
 const balancEl = document.querySelector(".balance .value")
-
 const incomeTotalEl = document.querySelector(".outcome-total")
-
 const outcomeTotalEl = document.querySelector(".income-total")
-
 const chartEl = document.querySelector(".chart")
+const expenseEl = document.querySelector("#expense")
+const incomeEl = document.querySelector("#income")
+const allEl = document.querySelector("#all")
+const incomeList = document.querySelector("#income .list")
+const expenseList = document.querySelector("#expense .list")
+const allList = document.querySelector("#all .list")
+
+// ================ Select btn ==================
 
 const expenseBtn = document.querySelector(".tab1")
 const incomeBtn = document.querySelector(".tab2")
 const allBtn = document.querySelector(".tab3")
 
-element.classList.remove("active");
-element.classList.add("active")
+// ================ Input btns ==================
 
-const incomeList = document.querySelector("#income .list")
-const expenseList = document.querySelector("#expense .list")
-const allList = document.querySelector("#all .list")
-
-const expenseEl = document.querySelector("#expense")
-const incomeEl = document.querySelector("#income")
-const allEl = document.querySelector("#all")
-
-element.classList.remove("hide");
-element.classList.add("hide")
+const addExpense = document.querySelector(".add-expense")
+const expenseTitle = document.querySelector("expense-title-input")
+const expenseAmount = document.querySelector("expense-amount-input")
 
 const addIncome = document.querySelector(".add-income")
 const incomeTitle = document.getElementById("income-title-input")
 const incomeAmount = document.getElementById("income-amount-input")
 
-
+// ==================== Event Listeners ===================
 
 expenseBtn.addEventListener('click', function () {
     active(expenseBtn);
@@ -49,8 +48,12 @@ allBtn.addEventListener('click', function () {
     hide([incomeEl, expenseEl])
 })
 
+// ================== Variables =======================
 
-function ative(element) {
+let ENTRY_LIST = [];
+
+
+function active(element) {
     element.classList.add("active")
 }
 
@@ -68,6 +71,30 @@ function inactive(elementsArray) {
         element.classList.remove("active")
     });
 }
+
+
+addIncome.addEventListener('click', function () {
+    if (!incomeTitle.value || !incomeAmount.value) return;
+    let income = {
+        type: "income",
+        title: incomeTitle.value,
+        amount: parseFloat(incomeAmount.value)
+    }
+    ENTRY_LIST.push(income)
+    updateUI();
+    clearInput([incomeTitle, incomeAmount])
+})
+addExpense.addEventListener('click', function () {
+    if (!expenseTitle.value || !expenseAmount.value) return;
+    let expense = {
+        type: "expense",
+        title: expenseTitle.value,
+        amount: parseFloat(expenseAmount.value)
+    }
+    ENTRY_LIST.push(expense)
+    updateUI();
+    clearInput([expenseTitle, expenseAmount])
+})
 
 
 
